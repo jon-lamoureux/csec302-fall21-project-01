8be500a52746958663d5cd935323192df8ce9eef
JBeisbier <69814354+JBeisbier@users.noreply.github.com>
2021-10-12 23:03:53 -0400

    16 finished

--- src/IDS16J/NonCompliant16.java
+++ src/IDS16J/NonCompliant16.java
@@ -4,12 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 public class NonCompliant16 {
-    private static void createXMLStreamBad(final BufferedOutputStream outStream,
-                                           final String quantity) throws IOException {
-        String xmlString = "<item>\n<description>Widget</description>\n"
+    public static String createXMLStreamBad(final String quantity) throws IOException {
+        return "<item>\n<description>Widget</description>\n"
                 + "<price>500</price>\n" + "<quantity>" + quantity
                 + "</quantity></item>";
-        outStream.write(xmlString.getBytes());
-        outStream.flush();
     }
 }
