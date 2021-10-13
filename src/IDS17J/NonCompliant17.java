e76fc6f2e5fb01cec8f32da80ea02b12359e97db
JBeisbier <69814354+JBeisbier@users.noreply.github.com>
2021-10-12 23:34:44 -0400

    17 Finished

--- src/IDS17J/NonCompliant17.java
+++ src/IDS17J/NonCompliant17.java
@@ -12,22 +12,11 @@ import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class NonCompliant17 {
+    public String resolveEntity(String publicId, String file) {
 
-    public static void receiveXMLStream(InputStream inStream,
-                                         DefaultHandler defaultHandler)
-            throws ParserConfigurationException, SAXException, IOException {
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        SAXParser saxParser = factory.newSAXParser();
-        saxParser.parse(inStream, defaultHandler);
+        System.out.println("Resolving entity: " + publicId + " " + file);
+        return file;
+
     }
-
-    public static void main(String[] args) throws ParserConfigurationException,
-            SAXException, IOException {
-        try {
-            receiveXMLStream(new FileInputStream("evil.xml"), new DefaultHandler());
-        } catch (java.net.MalformedURLException mue) {
-            System.err.println("Malformed URL Exception: " + mue);
-        }
-    }
-
 }
+
